6d0edfd69e898b53afdee14fbeee263eab330835
Duane David <duanedave17@gmail.com>
2023-01-21 10:53:57 -0800

    Add api endpoint

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,34 @@
-import { configureStore, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  createSlice,
+  createAsyncThunk,
+} from '@reduxjs/toolkit';
 
-const fetchGreetings = createAsyncThunk("greetings/fetchGreetings", async () => {});
+const fetchGreetings = createAsyncThunk(
+  'greetings/fetchGreetings',
+  async () => {
+    const response = await fetch(
+      'https://greeting-t0aj.onrender.com/greetings'
+    );
+    const data = await response.json();
+    return data;
+  }
+);
 
-const greetingsSlice = createSlice({});
+const greetingsSlice = createSlice({
+  name: 'greetings',
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) =>
+    builder.addCase(fetchGreetings.fulfilled, (_state, action) => {
+      return action.payload;
+    }),
+});
 
 const store = configureStore({
-    reducer: {
-        greetings: greetingsSlice.reducer,
-    },
+  reducer: {
+    greetings: greetingsSlice.reducer,
+  },
 });
 
 export default store;
